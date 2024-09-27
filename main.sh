bf937fc3e5457fe79bca84987a013d7a294b50de
Miguel-Sanchez241001 <msanchez24@autonoma.edu.pe>
2024-09-27 15:37:27 -0500

    Data sate v4

--- main.sh
+++ main.sh
@@ -2,8 +2,8 @@
 
 # Definir los parámetros
 DB_CONNECTION="bn_sate/bn_sate@//10.7.12.177:1521/orades"
-PATH_FILE="logs/FICTA19240301.txt"
-PATH_FILE_FAIL="logs/FICTA19240301_FAIL.TXT"
+PATH_FILE="logs/FICTA19240214.txt"
+PATH_FILE_FAIL="logs/FICTA19240214_fail.TXT"
 TYPE_PROCESS="1"
 TYPE_PROCESSMC="FICTA"
 # Ejecutar el JAR con los parámetros
